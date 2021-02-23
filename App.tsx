97532e7a86023f648c9d57d6cf8aa6f32d1328ba
Osinakayah Ifeanyi <ifeanyiosinakayah15@gmail.com>
2021-02-24 00:30:08 +0100

    Integrated chart into react native app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, Dimensions } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+const windowWidth = Dimensions.get('window').width;
+export default class App extends React.Component {
+  render() {
+      return <WebView
+        onError={(syntheticEvent) => {
+          const { nativeEvent } = syntheticEvent;
+          console.warn('WebView error: ', nativeEvent);
+        }}
+        source={require("./dist/index.html")}
+        // source={{uri: 'https://nairaland.com'}}
+        onHttpError={(syntheticEvent) => {
+          const { nativeEvent } = syntheticEvent;
+          console.warn(
+              'WebView received error status code: ',
+              nativeEvent.statusCode,
+          );
+        }}
+        allowFileAccess={true}
+        allowFileAccessFromFileURLs={true}
+        allowUniversalAccessFromFileURLs={true}
+        renderError={(errorName) => <Text>{errorName}</Text>}
+        style={{ marginTop: 30, width: windowWidth}} />;
+  }
 }
 
 const styles = StyleSheet.create({
